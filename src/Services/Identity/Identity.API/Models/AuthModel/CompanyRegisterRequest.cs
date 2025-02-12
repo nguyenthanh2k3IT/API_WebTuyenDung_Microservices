c046870c2af89219d1d875437bafe0a7df5ad5e5
nguyenquynhthanh2k3 <nguyenquynhthanh2k3@gmail.com>
2025-02-13 01:41:25 +0700

    update

---
+++ src/Services/Identity/Identity.API/Models/AuthModel/CompanyRegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Identity.API.Models.AuthModel;
+
+public class CompanyRegisterRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Fullname { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public Guid SizeId { get; set; }
+    public List<string> ProvinceIds { get; set; } = new List<string>();
+}
